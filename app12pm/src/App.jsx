883678ed1c658a933208d15c1eb3e00345b85b0e
vikash sharma <vikashtiwari9893@gmail.com>
2024-06-20 16:15:31 +0530

    second commit

--- app12pm/src/App.jsx
+++ app12pm/src/App.jsx
@@ -1,31 +1,120 @@
-import Employee from "./Employee";
 
-const nm= "rahul";
-const cls="Mtech";
-const fees=450000;
 
-const App=()=>{
+// map method
+/*const age =[24,20,33,34,50];
+const myage= age.map((key)=>{
   return(
     <>
-    <h1>welcome</h1>
-    <Employee stunm={nm}  cls={cls}  stufees={fees}/>
+
+    <h1>{key*2}</h1>
     </>
   )
+
+})*/
+
+
+//option and select tag
+/*const city= ["bhopal","indore","rewa","shahdol"];
+const myage=city.map((key)=>{
+return(
+  <>
+    <option>{key}</option>
+
+  </>
+)
+
+
+})*/
+
+
+const subject=["php","oracle","java","phython","react"];
+const mysub=subject.map((key)=>{
+  return(
+    <>
+    <li>{key}</li>
+    </>
+  )
+})
+
+const App=()=>{
+ return(
+  <>
+  <h1>welcome to cybrom</h1>
+  <ul>
+    {mysub}
+  </ul>
+  </>
+ )
 }
 
 
-
-
-
 /*const App=()=>{
-  return(
-    <>
-    <h1>welcome to cybrom</h1>
-    <Employee  nm="Arun"  empno="E007"  sal="320000"/>
-    </>
-  )
-  
+return(
+  <>
+  <h1>welcome to cybrom</h1>
+  <select>
+  {myage}
+  </select>
+  </>
+)
 }*/
 
 
-export default App;
+
+
+
+
+
+
+
+// map method
+/*const App=()=>{
+  const name=["ram","shyam","mohan","sohan"];
+  const ans =name.map((key)=>{
+    return(
+      <>
+      <h1>{key}</h1>
+      </>
+    )
+  })
+
+  return(
+    <>
+    <h1>wlecome to cybrom</h1>
+    {ans}
+
+    </>
+  )
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+// props
+// import Student from "./Student"
+
+// const data={
+//   "rollno":121,
+//   "name": "rahul",
+//   "city": "bhopal",
+// }
+
+// const App=()=>{
+//   return(
+//     <>
+
+//     <h1>welcome to cybrom</h1>
+//     <Student rno={data.rollno} nm={data.name} ct={data.city}/>
+//     </>
+//   )
+// }
+
+ export default App;
---
+++ app12pm/src/Student.jsx
@@ -0,0 +1,12 @@
+
+const Student=(props)=>{
+return(
+    <>
+    <h1>Student rollno: {props.rno}</h1>
+    <h1>name: {props.nm}</h1>
+    <h1>city: {props.ct}</h1>
+    </>
+)
+}
+
+export default Student;
