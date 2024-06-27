c85efbf9848d475f1dc04b0febfb73f28a17b1b8
vikash sharma <vikashtiwari9893@gmail.com>
2024-06-27 20:35:40 +0530

    eight commit

--- app12pm/src/App.jsx
+++ app12pm/src/App.jsx
@@ -1,407 +1,147 @@
-/*import Studentss from "./Studentss";
-import Newcybrom from "./Newcybrom";
+/*import myimg1 from "./images/b.jpg.jpeg";
+import myimg2 from "./images/blue4.jpg.jpeg";
+import myimg3 from "./images/blue5.jpg.jpeg";*/
 
-const ans= Studentss.map((key)=> <Newcybrom rno={key.rollno} nm={key.name} ct={key.city}  fs={key.fees} />);
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+
 
 const App=()=>{
   return(
-    <>
-    <h1 align="center">Our student list</h1>
-    <table border="2" bgcolor="blue"  align="center"  width="500">
-     <tr bgcolor="red">
-      <th>Roll no</th>
-      <th>Name</th>
-      <th>City</th>
-      <th>Fees</th>
-     </tr>
-     {ans}
-    </table>
-
-    </>
-  )
-}*/
-
-//import { Container } from "react-bootstrap";
-
-
-/*const App=()=>{
-  return(
    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-   <h1  style={{color:"red", fontFamily:"verdana",   }}>welcome to cybrom</h1>
-   <h2 style={{color: "blue"}}>hello</h2>
+
+    <Carousel>
+      <Carousel.Item>
+        <img src="public/b1.jpg"  width="100%"   height="50%"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="public/b2.jpg"  width="100%"   height="200" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="public/b3.jpg" width="100%"   height="50%"/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    <br/>
+    <br/>
+
+
+    <Container fluid="md">
+      <Row>
+        <Col>
+        <Card style={{ width: '14rem' }}>
+        <img src="public/b1.jpg"/>
+      
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+
+        <Col>
+        <Card style={{ width: '14rem' }}>
+     <img src="public/b2.jpg"/>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+
+
+        <Col>
+        <Card style={{ width: '14rem' }}>
+        <img src="public/b3.jpg"/>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+
+        <Col>
+        <Card style={{ width: '14rem' }}>
+        <img src="public/b3.jpg"/>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+      </Row>
+    </Container>
+
+
    
    </>
 
   )
-}*/
+}
 
-
-
-/*const App=()=>{
-  return(
-    // <h1 style={{width:"40px", color:"orange"}}>helo</h1>
-
-    <>
-    <div style={{color:"red", border:"10px",width:"200px", height:"200px", backgroundColor:"red", borderRadius:"30%",borderColor:"teal"}}>
-      <div style={{ marginTop:"50%", marginLeft:"50%",color:"blue", border:"10px",width:"80px", height:"80px", backgroundColor:"yellow", borderRadius:"30%",borderColor:"black"}}>
-        <div style={{color:"pink", border:"10px",width:"40px", height:"40px", backgroundColor:"pink", borderRadius:"30%",borderColor:"teal"}}>
-
-        </div>
-      </div>
-    </div>
-    </>
-
-  )
-}*/
-
-// button bootstrap
-// import Button from 'react-bootstrap/Button';
-
-// const App=()=>{
-//   return(
-//     <>
-    
-//     <h1>welcome to bootstrap</h1>
-//     <Button variant="primary">Primary</Button>
-//     <br/>
-//     <Button variant="danger">Primary</Button>
-//     <br/>
-//     <Button variant="info">Primary</Button>
-//     <br/>
-//     <Button variant="warning">Primary</Button>
-//     <br/>
-//     </>
-//   )
-// }
-
-
-// alert
-// import Alert from 'react-bootstrap/Alert';
-//  import {Container} from "react-bootstrap";
-
-//    const App=()=>{
-//     return(
-//       <>
-//       <Container>
-//       <Alert  variant="danger"  dismissible>
-//       This is alert box
-//     </Alert>
-//     <br/>
-//     <Alert  variant="info"  dismissible>
-//       This is alert box
-//     </Alert>
-
-
-//       </Container>
-      
-//       </>
-//     )
-//    }
-
-// badges in bootstrap
-
-/*import Badge from 'react-bootstrap/Badge';
-
-   const App=()=>{
-     return(
-      <>
-       <h1>
-        Example heading <Badge bg="secondary">New</Badge>
-      </h1>
-
-      <h1>
-        Example heading <Badge bg="primary">New</Badge>
-      </h1>
-
-      <h1>
-        Example heading <Badge bg="danger">New</Badge>
-      </h1>
-
-      <h1>
-        Example heading <Badge bg="info">New</Badge>
-      </h1>
-      
-      
-      </>
-
-     )
-   }*/
-
-
-      // button 
-     /*import Button from 'react-bootstrap/Button';
-     import ButtonGroup from 'react-bootstrap/ButtonGroup';
-
-     const App=()=>{
-      return(
-       <>
-        <ButtonGroup aria-label="Basic example">
-      <Button variant="secondary">Left</Button>
-      <Button variant="secondary">Middle</Button>
-      <Button variant="secondary">Right</Button>
-    </ButtonGroup>
-       
-       </>
-
-
-      )
-     }*/
-
-
-      // dropdown
-      /*import Dropdown from 'react-bootstrap/Dropdown';
-
-
-      const App=()=>{
-        return(
-          <>
-
-<Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-          </>
-        )
-
-      }*/
-
-
-// navbar 
-/*import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-const App=()=>{
-  return(
-   <>
-   <Navbar bg="danger" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br/>
-
-      <Navbar bg="info" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-   </>
-
-  )
-}*/
-
-/*import Spinner from 'react-bootstrap/Spinner';
-
- const App=()=>{
-  return(
-    <>
-     <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    
-    </Spinner>
-    
-    </>
-  )
- }*/
-
-
-  /*import Table from 'react-bootstrap/Table';
-
-  const App=()=>{
-    return(
-
-      <>
-
-<Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-      
-      </>
-    )
-
-  }*/
-
-    import { Container, Row,Col } from "react-bootstrap";
-    /*const App=()=>{
-      return(
-        <>
-          <Container className="text-center  border bg-info pt-1">   
-
-            <h1>welcome to cybrom Bhoapl</h1>
-          </Container>
-        
-        </>
-
-      )
-    }*/
-
-      //import { Container, Row,Col } from "react-bootstrap";
-
-      /*const App=()=>{
-        return(
-
-          <>
-          <Container   className="border text-center my-5 p-5">
-            <Row>
-            <Col  className="border bg-danger p-5 text-white fs-1">
-               hello we are web developer
-            
-            </Col>
-
-            <Col  className="border bg-success p-5 text-white fs-1">
-            
-               welcome to cybrom
-            
-            </Col>
-
-
-            </Row>
-           
-
-          </Container>
-          
-          </>
-        )
-      }*/
-
-        // maximum md  12     
-    //import { Container, Row,Col } from "react-bootstrap";
-      // const App=()=>{
-      //   return(
-      //    <>
-      //    <Container fluid  className="border p-5 bg-info">
-      //     <Row>
-
-      //       <Col md={2} className="bg-danger  text-center  text-white fs-4 p-5">
-      //           hello react
-      //       </Col>
-      //       <Col md={4} className="bg-success  text-center  text-white fs-4 p-5">
-      //       hello react
-            
-      //       </Col>
-
-      //       <Col md={2} className="bg-dark  text-center  text-white fs-4 p-5">
-      //       hello react
-            
-      //       </Col>
-
-      //       <Col md={4} className="bg-primary  text-center  text-white fs-4 p-5">
-      //       hello react
-            
-      //       </Col>
-      //     </Row>
-
-
-
-      //    </Container>
-         
-         
-      //    </>
-
-      //   )
-      // }
- 
-    //import { Container, Row,Col } from "react-bootstrap";
-         // double row and coloumn
-      const App=()=>{
-        return(
-         <>
-         <Container fluid  className="border p-5 bg-info">
-          <Row>
-
-            <Col  className="bg-danger  text-center  text-white fs-4 p-5">
-                hello react
-            </Col>
-            <Col  className="bg-success  text-center  text-white fs-4 p-5">
-            hello react
-            
-            </Col>
-
-            <Col  className="bg-dark  text-center  text-white fs-4 p-5">
-            hello react
-            
-            </Col>
-
-           
-          </Row>
-
-        <br/>
-
-          <Row>
-
-            <Col md={6} className="bg-danger  text-center  text-white fs-4 p-5">
-                hello react
-            </Col>
-            <Col md={3} className="bg-success  text-center  text-white fs-4 p-5">
-            hello react
-            
-            </Col>
-
-            <Col md={3} className="bg-dark  text-center  text-white fs-4 p-5">
-            hello react
-            
-            </Col>
-
-           
-          </Row>
-
-
-
-         </Container>
-         
-         
-         </>
-
-        )
-      }
-
-
-
-
- export default App;
+export default App;
