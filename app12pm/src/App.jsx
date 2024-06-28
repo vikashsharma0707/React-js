14e1abc638b7be01a7a57550c141ede8b27ff330
vikash sharma <vikashtiwari9893@gmail.com>
2024-06-28 17:35:54 +0530

    ninth commit

--- app12pm/src/App.jsx
+++ app12pm/src/App.jsx
@@ -1,68 +1,52 @@
-/*import myimg1 from "./images/b.jpg.jpeg";
-import myimg2 from "./images/blue4.jpg.jpeg";
-import myimg3 from "./images/blue5.jpg.jpeg";*/
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import Carousel from 'react-bootstrap/Carousel';
 
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
 
-
 const App=()=>{
-  return(
-   <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+    return(
+     <>
+      <Navbar   Text="center" bg="danger" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
 
 
-    <Carousel>
+      <Carousel slide={false}>
       <Carousel.Item>
-        <img src="public/b1.jpg"  width="100%"   height="50%"/>
+       <img src="public/b1.jpg"  width="100%"  height="300"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="public/b2.jpg"  width="100%"   height="200" />
+      <img src="public/b2.jpg"  width="100%"  height="300"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="public/b3.jpg" width="100%"   height="50%"/>
+      <img src="public/b3.jpg"  width="100%"  height="300"/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -71,10 +55,11 @@ const App=()=>{
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+
     <br/>
     <br/>
 
-
+    
     <Container fluid="md">
       <Row>
         <Col>
@@ -106,7 +91,6 @@ const App=()=>{
     </Card>
         </Col>
 
-
         <Col>
         <Card style={{ width: '14rem' }}>
         <img src="public/b3.jpg"/>
@@ -137,11 +121,10 @@ const App=()=>{
       </Row>
     </Container>
 
+     
+     </>
 
-   
-   </>
-
-  )
+    )
 }
 
 export default App;
